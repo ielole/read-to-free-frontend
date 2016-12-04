5a4bd2b918209c40ff99a000a840eb9ef6282466
ielole <ieloletin@gmail.com>
2016-12-04 01:50:24 -0500

    Update host.
    Linked to back end.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-  host:'http://localhost:4741'
+  host:'https://read-to-free.herokuapp.com/'
   // need to put http:// in front, even with localhost or you get a cross origins request error
 };
 
