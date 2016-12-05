1d581033c8eebd7fadfb328d85328e5b5f454b54
ielole <ieloletin@gmail.com>
2016-12-05 07:44:36 -0500

    Review crud working.
    Deployment for presentation.

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(46);
+	module.exports = __webpack_require__(47);
 
 
 /***/ },
@@ -10332,7 +10332,7 @@
 
 /***/ },
 
-/***/ 46:
+/***/ 47:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
